67db5c925d7497f14ae4b4018cac8b08e482bdef
Abuddabi <web.abudabi@gmail.com>
2024-01-13 14:03:51 -0600

    completed W02 Prepare: Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -2,8 +2,25 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning02 World!");
-    }
+  static void Main(string[] args)
+  {
+    Job job1 = new Job();
+    job1._jobTitle = "Software Engineer";
+    job1._company = "Tesla";
+    job1._startYear = 3000;
+    job1._endYear = 3003;
+
+    Job job2 = new Job();
+    job2._jobTitle = "CEO";
+    job2._company = "SpaceX";
+    job2._startYear = 3003;
+    job2._endYear = 3010;
+
+    Resume resume = new Resume();
+    resume._name = "Egor Sotnikov";
+    resume._jobs.Add(job1);
+    resume._jobs.Add(job2);
+
+    resume.Display();
+  }
 }
