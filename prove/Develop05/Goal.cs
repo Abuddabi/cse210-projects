2bd76b0e8ad1456120d817709b38ad31dbf594fb
Abuddabi <web.abudabi@gmail.com>
2024-02-02 19:13:08 -0600

    Goal Tracker. The design

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,26 @@
+using System;
+
+abstract class Goal
+{
+  private string _shortName;
+  private string _description;
+  private string _points;
+
+  public Goal(string name, string description, string points)
+  {
+    _shortName = name;
+    _description = description;
+    _points = points;
+  }
+
+  public abstract void RecordEvent();
+
+  public abstract bool IsComplete();
+
+  public abstract string GetStringRepresentation();
+
+  public virtual string GetDetailsString()
+  {
+    return "";
+  }
+}
